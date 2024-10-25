d15e50b8d5615a24de03fbc20a5a414f417b7c26
QianGuoZi <lianghuaqian@163.com>
2024-10-25 17:13:05 +0800

    scheduler

---
+++ scheduler/scheduler.py
@@ -0,0 +1,8 @@
+#接收资源需求，输出部署方案
+
+class Scheduler(object):
+    def __init__(self) -> None:
+        pass    
+    def resource_schedule():
+        return
+    
---
+++ scheduler/task_analyzer.py
@@ -0,0 +1,13 @@
+# 接收用户请求（包括数据集、dml代码、manager.py、节点信息、网络拓扑）
+# 分析用户请求，将资源请求的内容丢给scheduler，从scheduler中拿到部署方案
+# 将方案丢给edgetb部署
+
+class TaskAnalyzer(object):
+    def __init__(self) -> None:
+        pass
+    def receive_request():
+        return 
+    def analyse_request():
+        return
+    def send_request_edgetb():
+        return
